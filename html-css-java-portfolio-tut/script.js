c1ee9f6d4c10177d07e9c2510eca68c30fc132d0
Krissy05-TD <krisdavids2005@gmail.com>
2026-03-01 17:23:11 +0200

    overlap fixed - everything complete

--- html-css-java-portfolio-tut/script.js
+++ html-css-java-portfolio-tut/script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+  if (dots.style.display === "none") {
+    dots.style.display = "inline"; btnText.innerHTML = "Read more"; moreText.style.display = "none";
+  } else {
+    dots.style.display = "none"; btnText.innerHTML = "Read less"; moreText.style.display = "inline";
+  }
+}
